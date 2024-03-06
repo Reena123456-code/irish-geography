cbd04fe689d92ea6a49219a3a4e5af0ccd386d54
Reena123456-code <reenaorourke@gmail.com>
2024-03-06 20:34:43 +0000

    Add function random number + function countyName

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,118 +4,60 @@ document.addEventListener("DOMContentLoaded", function () {
 
     for (let button of buttons) {
         button.addEventListener("click", function () {
-            if (this.getAttribute("data-type") === "start") {
+            if (this.getAttribute("button") === "start") {
                 alert(county());
             }
-        }
-        }
-}
+        })
+    }
 })
 
 // selecting county using math random
-function runGame() {
-    let county() = Math.floor(Math.random() * 32) + 1;
+function randomNumber(min, max) {
+    min = Math.ceil(1);
+    max = Math.floor(32);
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 // matching numbers to counties using switch
-switch (numbers) {
-    case 1:
-        county = "Antrim";
-        break;
-    case 2:
-        county = "Armagh";
-        break;
-    case 3:
-        county = "Carlow";
-        break;
-    case 4:
-        county = "Cavan";
-        break;
-    case 5:
-        county = "Clare";
-        break;
-    case 6:
-        county = "Cork"; 
-        break;
-    case 7:
-        county = "Donegal"; 
-        break;
-    case 8:
-        county = "Down";
-        break;
-    case 9:
-        county = "Dublin";
-        break;
-    case 10:
-        county = "Fermanagh";
-        break;
-    case 11:
-        county = "Galway";
-        break;
-    case 12:
-        county = "Kerry";
-        break; 
-    case 13:
-        county = "Kildare";
-        break;
-    case 14:
-        county = "Kilkenny";
-        break;
-    case 15:
-        county = "Laois";
-        break;
-    case 16:
-        county = "Leitrim";
-        break;
-    case 17:
-        county = "Limerick";
-        break;
-    case 18:
-        county = "Londonderry";
-        break;
-    case 19:
-        county = "Longford";
-        break;
-    case 20:
-        county = "Louth";
-        break;
-    case 21:
-        county = "Mayo";
-        break;
-    case 22:
-        county = "Meath";
-        break;
-    case 23:
-        county = "Monaghan";
-        break;
-    case 24:
-        county = "Offaly";
-        break;
-    case 25:
-        county = "Roscommon";
-        break;
-    case 26:
-        county = "Sligo";
-        break;
-    case 27:
-        county = "Tipperary";
-        break;
-    case 28:
-        county = "Tyrone";
-        break;
-    case 29:
-        county = "Waterford";
-        break;
-    case 30:
-        county = "Westmeath";
-        break;
-    case 31:
-        county = "Wexford";
-        break;
-    case 32:
-        county = "Wicklow";
-        break;
+function countyName(county1, num) {
+    let county = {
+        Antrim: 1,
+        Armagh: 2,
+        Carlow: 3,
+        Cavan: 4,
+        Clare: 5,
+        Cork: 6,
+        Donegal: 7,
+        Down: 8,
+        Dublin: 9,
+        Fermanagh: 10,
+        Galway: 11,
+        Kerry: 12,
+        Kildare: 13,
+        Kilkenny: 14,
+        Laois: 15,
+        Leitrim: 16,
+        Limerick: 17,
+        Londonderry: 18,
+        Longford: 19,
+        Louth: 20, 
+        Mayo: 21,
+        Meath: 22,
+        Monaghan: 23,
+        Offaly: 24,
+        Roscommon:25,
+        Sligo :26,
+        Tipperary: 27,
+        Tyrone: 28,
+        Waterford: 29,
+        Westmeath: 30,
+        Wexford: 31,
+        Wicklow: 32,
+    }
+    return county;
 }
+console.log(countyName);
+
 
 function checkAnswer() {
 
