583d7c0cb3836ff17b95d09944262ab4a69c7e02
Reena123456-code <reenaorourke@gmail.com>
2024-03-20 10:30:14 +0000

    Getting Counties by math random to run

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,32 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // selecting county using math random
-function randomNumber() {
-    let exists = []
-    randomNumber:
-    for (let i = 1; i <= 32; i++) {
-        do {
-            randomNumber = Math.floor(Math.random() * 32) + 1;
-        } while (exists[randomNumber]);
-        exists[randomNumber] = true;
-        alert(randomNumber);
-    }
-}
+//function randomNumber() {
+//   let exists = []
+//   randomNumber:
+//    for (let i = 1; i <= 32; i++) {
+//        do {
+//            randomNumber = Math.floor(Math.random() * 32) + 1;
+//       } while (exists[randomNumber]);
+//       exists[randomNumber] = true;
+//       alert(randomNumber);
+//    }
+//}
 
 // matching numbers to counties using switch
 function county() {
     const counties = ["Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare",
         "Kilkenny", "Laois", "Leitrim", "Limerick", "Londonderry", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon",
         "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"];
-    console.log(county());
+
 }
+
+function counties() {      
+    alert(county[(Math.floor(Math.random() * county.length))]);
+}
+counties()
+
+
 
 function checkAnswer() {
     if (county() = True) {
