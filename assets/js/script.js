d1cb73b427c01755e61efe7efe02049e995feb7d
Reena123456-code <reenaorourke@gmail.com>
2024-03-20 12:22:26 +0000

    Starting to put input for county section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,17 @@
 // Loading game to dom before starting game
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("btn").addEventListener("click", function () );
+    document.getElementById("btn").addEventListener("click", function() );
 });
 
-// selecting county using math random
-//function randomNumber() {
-//   let exists = []
-//   randomNumber:
-//    for (let i = 1; i <= 32; i++) {
-//        do {
-//            randomNumber = Math.floor(Math.random() * 32) + 1;
-//       } while (exists[randomNumber]);
-//       exists[randomNumber] = true;
-//       alert(randomNumber);
-//    }
-//}
+
 
 // matching numbers to counties using switch
-function county() {
-    const counties = ["Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare",
-        "Kilkenny", "Laois", "Leitrim", "Limerick", "Londonderry", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon",
-        "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"];
 
-}
+const county = ["Antrim", "Armagh", "Carlow", "Cavan", "Clare", "Cork", "Donegal", "Down", "Dublin", "Fermanagh", "Galway", "Kerry", "Kildare",
+    "Kilkenny", "Laois", "Leitrim", "Limerick", "Londonderry", "Longford", "Louth", "Mayo", "Meath", "Monaghan", "Offaly", "Roscommon",
+    "Sligo", "Tipperary", "Tyrone", "Waterford", "Westmeath", "Wexford", "Wicklow"];
 
-function counties() {      
+function counties() {
     alert(county[(Math.floor(Math.random() * county.length))]);
 }
 counties()
@@ -32,6 +19,7 @@ counties()
 
 
 function checkAnswer() {
+
     if (county() = True) {
         alert("correct");
     } else {
